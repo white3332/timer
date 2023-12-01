3ddb0fc270b009f418cf0af6017314c390230f55
white3332 <129389029+white3332@users.noreply.github.com>
2023-12-01 23:03:55 +0900

    1201

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let timer;
-var timeLeftSec = 3; // 초
+var timeLeftSec = 60 // 초
+var countIndex = 1
 var timerRunning = false;
 
 // 핵심 // 타이머를 중지하거나 이어서 실행함
+// (updateTimer, 10) <- 0.01 초에 1회 실행
 function startStopTimer() {
     const button = document.getElementById('startStopButton');
 
@@ -11,41 +13,46 @@ function startStopTimer() {
         button.innerText = '이어서 시작';
     } else {
         if (timer == undefined || timer >= 1) {
-            timer = setInterval(updateTimer, 1000);
+            timer = setInterval(updateTimer, 100);
             button.innerText = '중지';
         }
     }
-    
+
     timerRunning = !timerRunning;
 }
 
-// 타이머 시분초 표시
+// 타이머 시분초 표시 1초에 1번 반복하는 함수
 function updateTimer() {
+    
     if (timeLeftSec > 0) {
         // 타이머 실행중일 때
         timeLeftSec--;
         const hours = Math.floor(timeLeftSec / 3600);
         const minutes = Math.floor(timeLeftSec / 60);
         const seconds = timeLeftSec % 60;
-        document.getElementById('timer').innerText = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        const displayTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        document.getElementById('timer').innerText = displayTime;
+        notificationAfterTimeDetection(displayTime);
         document.getElementById('startStopButton').disabled = false;
     } else if ((timeLeftSec <= 0) && (false)) {
         // 타이머 종료했을 때
         clearInterval(timer);
         document.getElementById('timer').innerText = '00:00:00';
+        notificationAfterTimeDetection('00:00:00');
         document.getElementById('startStopButton').disabled = true;
     }
 }
 
-// 타이머 초기화 및 재시작(이어서 시작 X)
-function restart(){
+// 타이머 시간 초기화 및 재시작(이어서시작 X)
+function restart() {
     // 시 분 초 세팅
-    lastElement = accumulatetimes[accumulatetimes.length - 1]; // 수정 필요
+    const lastElement = accumulateTimes[accumulateTimes.length - 1]; // 수정 필요
     const hours = Math.floor(lastElement / 3600);
     const minutes = Math.floor(lastElement / 60);
     const seconds = lastElement % 60;
     document.getElementById('timer').innerText = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     timeLeftSec = lastElement
+    countIndex = 1;
     // 타이머 작동 명령
     if (timerRunning) {
         // pass
@@ -54,18 +61,88 @@ function restart(){
     }
 }
 
+// 타이머 알람
+function notificationAfterTimeDetection(displayTime) {
+    // 경과된 시간이 배열에 입력된 시간과 일치하는지 확인하고 알림 출력
+    const lastElementTime = accumulateTimes[accumulateTimes.length - 1];
+    const elapseTime = lastElementTime - timeLeftSec;
+    if (elapseTime >= accumulateTimes[countIndex - 1]) {
+        openNotificationModal();
+        countIndex ++;
+    }
+}
 
 // 모달을 열기
-function openModal() {
+function openEditModal() {
     const modalWrap = document.getElementById('modalWrap');
     modalWrap.style.display = 'block';
+    timeFormReboot()
 }
 // 모달을 숨기기
-function closeModal() {
+function closeEditModal() {
     const modalWrap = document.getElementById('modalWrap');
     modalWrap.style.display = 'none';
 }
+// 모달을 열기
+function openNotificationModal() {
+    const modalWrap = document.getElementById('modalNotificationWrap');
+    modalWrap.style.display = 'block';
 
+    const accTimes = [];
+    for (let i = 0; i < accumulateTimes.length; i++) {
+        let seconds = accumulateTimes[i];
+        let minutes = 0;
+        let hours = 0;
+        // 분, 초가 60을 초과할때
+        if (seconds >= 60) {
+            minutes = Math.floor(seconds / 60) + minutes;
+            seconds = seconds % 60;
+        }
+        if (minutes >= 60) {
+            hours = Math.floor(minutes / 60) + hours;
+            minutes = minutes % 60;
+        }
+
+        // 시, 분, 초가 한 자리 숫자일 경우 앞에 0을 추가
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        accTimes.push(hours + ":" + minutes + ":" + seconds);
+    }
+
+
+    // 이전 시간을 입력
+    const strTimeElement = document.getElementById('strTime');
+    if (accTimes[countIndex -1] !== undefined && accTimes[countIndex -1] !== null && (countIndex -1 ) >= 0 ) {
+        strTimeElement.innerText = accTimes[countIndex -1];
+    } else {
+        // 값이 존재하지 않으면 '시작' 출력
+        strTimeElement.innerText = '시작';}
+
+    // 현재 시간을 입력
+    const curTimeElement= document.getElementById('curTime');
+    if (accTimes[countIndex] !== undefined && accTimes[countIndex] !== null) {
+        curTimeElement.innerText = accTimes[countIndex];
+    } else {
+        // 값이 존재하지 않으면 '시작' 출력
+        curTimeElement.innerText = '끝';}
+
+    // 끝 시간을 입력
+    const endTimeElement = document.getElementById('endTime');
+    if (accTimes[countIndex+1] !== undefined && accTimes[countIndex+1] !== null) {
+        endTimeElement.innerText = accTimes[countIndex+1];
+    } else {
+        // 값이 존재하지 않으면 '끝' 출력
+        endTimeElement.innerText = '끝';}
+}
+
+
+// 모달을 숨기기
+function closeNotificationModal() {
+    const modalWrap = document.getElementById('modalNotificationWrap');
+    modalWrap.style.display = 'none';
+}
 
 
 // 서브 페이지
@@ -75,20 +152,20 @@ function timeadjust(dir, timeinput) {
     // 시간 값 가져오기
     var currentValue = parseInt(document.getElementById(timeinput).value);
     // 값 감소 및 증가
-    if (dir == 'left') {var newValue = currentValue - 1;}
-    if (dir == 'right'){var newValue = currentValue + 1;}
+    if (dir == 'left') { var newValue = currentValue - 1; }
+    if (dir == 'right') { var newValue = currentValue + 1; }
     // 마이너스 값이면 0으로 변환
-    if (currentValue < 0) {currentValue = 0}
+    if (currentValue < 0) { currentValue = 0 }
     // 값을 다시 입력 필드에 설정
     document.getElementById(timeinput).value = newValue;
 }
 // 시간을 저장할 배열
-// 예시 [300, 300, 300]
-var times = [];
+// 예시 ['00:05:00', '00:05:00', '00:05:00']
+var times = ['00:00:30', '00:00:30'];
 // 누적시간을 저장할 배열
 // 메인 페이지에서 사용할 배열
 // 예시 [300, 600, 900]
-var accumulatetimes = [];
+var accumulateTimes = [30, 60];
 
 // 시간 단계를 추가함
 function addTime() {
@@ -99,9 +176,9 @@ function addTime() {
     var hours = parseInt(document.getElementById("in-hour").value, 10);
 
     // 시, 분, 초가 마이너스 값일때
-    if (seconds < 0) {seconds = 0}
-    if (minutes < 0) {minutes = 0}
-    if (hours < 0) {hours = 0}
+    if (seconds < 0) { seconds = 0 }
+    if (minutes < 0) { minutes = 0 }
+    if (hours < 0) { hours = 0 }
 
     // 분, 초가 60을 초과할때
     if (seconds >= 60) {
@@ -125,90 +202,51 @@ function addTime() {
     // 모든 시간을 누적한 값 생성
     var totalSeconds = 0;
     // 누적시간을 저장할 배열
-    accumulatetimes = [0];
-    for (var i = 0; i < times.length; i++) {
+    accumulateTimes = [0];
+    for (let i = 0; i < times.length; i++) {
         // 시, 분, 초를 초로 변환하여 더함
-        var timeParts = times[i].split(":");
-        var timeInSeconds = parseInt(timeParts[0],10) * 3600 + parseInt(timeParts[1],10) * 60 + parseInt(timeParts[2],10);
+        let timeParts = times[i].split(":");
+        let timeInSeconds = parseInt(timeParts[0], 10) * 3600 + parseInt(timeParts[1], 10) * 60 + parseInt(timeParts[2], 10);
         totalSeconds += timeInSeconds;
-        accumulatetimes.push(totalSeconds);
+        accumulateTimes.push(totalSeconds);
     }
 
-
-    // 시간을 표시할 요소에 배열의 내용을 추가
-    // 체크박스와 이미지를 동적으로 생성하고 추가
-    var timeForm = document.getElementById("timeForm");
-
-    var checkboxContainer = document.createElement("div");
-    checkboxContainer.className = "checkbox-container";
-
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = "timeCheckbox";
-    checkbox.value = newTime;
-
-    var label = document.createElement("label");
-    label.innerHTML = newTime;
-
-    var image1 = document.createElement("img");
-    image1.src = "up-click.png";
-    image1.alt = "up";
-    image1.addEventListener("click", function() {
-        // 이미지 클릭 시, 체크박스와 라벨과 형제 요소와 교체
-        var sibling = checkboxContainer.previousElementSibling;
-        if (sibling) {
-            sibling.parentNode.insertBefore(checkboxContainer, sibling);
-        }
-    });
-
-    var image2 = document.createElement("img");
-    image2.src = "down-click.png";
-    image2.alt = "down";
-    image2.addEventListener("click", function() {
-        // 이미지 클릭 시, 체크박스와 라벨과 앞의 형제 요소와 교체
-        var sibling = checkboxContainer.nextElementSibling;
-        if (sibling) {
-            sibling.parentNode.insertBefore(sibling, checkboxContainer);
-        }
-    });
-
-    checkboxContainer.appendChild(image1);
-    checkboxContainer.appendChild(image2);
-    checkboxContainer.appendChild(checkbox);
-    checkboxContainer.appendChild(label);
-    timeForm.appendChild(checkboxContainer);
+    // 시간을 표시할 요소와 체크박스 초기화
+    timeFormReboot()
 }
 
 // 시간 단계를 삭제함
 function removeTime() {
     // 선택된 체크박스의 값을 가져와서 배열에서 제거
-    var checkboxes = document.querySelectorAll('input[name="timeCheckbox"]:checked');
-    checkboxes.forEach(function(checkbox) {
-        var selectedValue = checkbox.value;
-        var index = times.indexOf(selectedValue);
+    const checkboxes = document.querySelectorAll('input[name="timeCheckbox"]:checked');
+    checkboxes.forEach(function (checkbox) {
+        const selectedValue = checkbox.value;
+        const index = times.indexOf(selectedValue);
         if (index !== -1) {
             times.splice(index, 1);
         }
     });
 
-    // 시간을 표시할 요소와 체크박스 초기화
-    var timeForm = document.getElementById("timeForm");
-    timeForm.innerHTML = "";
-
     // 모든 시간을 누적한 값 생성
     var totalSeconds = 0;
     // 누적시간을 저장할 배열
-    accumulatetimes = [0];
-    for (var i = 0; i < times.length; i++) {
+    accumulateTimes = [0];
+    for (let i = 0; i < times.length; i++) {
         // 시, 분, 초를 초로 변환하여 더함
-        var timeParts = times[i].split(":");
-        var timeInSeconds = parseInt(timeParts[0],10) * 3600 + parseInt(timeParts[1],10) * 60 + parseInt(timeParts[2],10);
+        let timeParts = times[i].split(":");
+        let timeInSeconds = parseInt(timeParts[0], 10) * 3600 + parseInt(timeParts[1], 10) * 60 + parseInt(timeParts[2], 10);
         totalSeconds += timeInSeconds;
-        accumulatetimes.push(totalSeconds);
+        accumulateTimes.push(totalSeconds);
     }
 
-    // 배열의 내용을 다시 표시
-    // 남아있는 시간에 대해 체크박스와 이미지 재생성
+    // 시간을 표시할 요소와 체크박스 초기화
+    timeFormReboot()
+}
+
+// 시간을 표시할 요소와 체크박스 초기화
+function timeFormReboot() {
+    var timeForm = document.getElementById("timeForm");
+    timeForm.innerHTML = "";
     for (var i = 0; i < times.length; i++) {
         var checkboxContainer = document.createElement("div");
         checkboxContainer.className = "checkbox-container";
@@ -224,7 +262,7 @@ function removeTime() {
         var image1 = document.createElement("img");
         image1.src = "up-click.png";
         image1.alt = "up";
-        image1.addEventListener("click", function() {
+        image1.addEventListener("click", function () {
             // 이미지 클릭 시, 체크박스와 라벨과 형제 요소와 교체
             var sibling = checkboxContainer.previousElementSibling;
             if (sibling) {
@@ -234,7 +272,7 @@ function removeTime() {
         var image2 = document.createElement("img");
         image2.src = "down-click.png";
         image2.alt = "down";
-        image2.addEventListener("click", function() {
+        image2.addEventListener("click", function () {
             // 이미지 클릭 시, 체크박스와 라벨과 앞의 형제 요소와 교체
             var sibling = checkboxContainer.nextElementSibling;
             if (sibling) {
@@ -246,6 +284,23 @@ function removeTime() {
         checkboxContainer.appendChild(checkbox);
         checkboxContainer.appendChild(label);
         timeForm.appendChild(checkboxContainer);
+        }
     }
-}
 
+function fetchdata() {
+    const id = document.getElementById('in-id').innerText; // id
+    const dataArray = times;/* 배열 데이터 */
+
+    fetch('http://localhost:3000/saveData', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, dataArray }),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log(data.message); // 서버로부터의 응답 확인
+    })
+    .catch(error => console.error('Error:', error));
+}
--- server.js
+++ server.js
@@ -1,10 +1,44 @@
-const express = require('express');
-const app = express();
+// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.) 
+var http = require('http'); 
 
-app.use(express.static(__dirname)); // 현재 디렉토리를 정적 파일로 서빙
+// 2. http 모듈로 서버를 생성한다.
+//    아래와 같이 작성하면 서버를 생성한 후, 사용자로 부터 http 요청이 들어오면 function 블럭내부의 코드를 실행해서 응답한다.
+var server = http.createServer(function(request,response){ 
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('Hello node.js!!');
+
 });
 
+// 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
+server.listen(8080, function(){ 
+    console.log('Server is running...');
+});
+
+/*
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+// CORS 설정
+app.use(cors());
+
+// Body parsing middleware for JSON
+app.use(express.json());
+
+// 데이터 저장을 위한 임시 변수
+const dataStore = {};
+
+// POST 요청을 처리하는 엔드포인트
+app.post('/saveData', (req, res) => {
+    const { id, dataArray } = req.body;
+    dataStore[id] = dataArray;
+    res.json({ message: 'Data saved successfully' });
+});
+
+// 서버 시작
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+*/
